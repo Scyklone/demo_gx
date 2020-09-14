9d9755d9fdf3e8aadda3835b3ad42b6739cf4700
Gabriel Ferrer Torres <31068412+Scyklone@users.noreply.github.com>
2020-09-14 12:02:11 -0400

    add_app

---
+++ prototype/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './style.css';
+import './css/bootstrap.min.css'
+
+
+
+function App() {
+  return (
+    <html>
+      <header>
+        <title>Web Page</title>
+        
+      </header>
+      <body>
+        <div id="h_1">
+        <div><ul><a class="hL" href="#">Header</a></ul></div>
+          
+        </div>
+        <div id="mainbody" class="m_1">
+            <div id="container">
+            <div ><p>Hello world</p></div>
+            
+            </div>
+        </div>
+        <div id="footer" class="foot"><p>This is footer</p></div>
+      </body>
+    </html>
+  )
+}
+
+export default App;
